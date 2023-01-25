82807109f8f86f3fb46a9a853eac0a4bf18d21c7
Pulkit Goyal <pulkitmds@gmail.com>
2023-01-25 06:51:55 +0100

    update constants and config

--- constants.py
+++ constants.py
@@ -14,9 +14,22 @@ class DEVICE(object):
     # Maximum length of a long channel
     LS_MAX_DIST = 0.10 # m
 
+    # Time drift factor
+    TIME_DRIFT_FACTOR = 1.01 
+
     # Additional device info
     INFO = {
         'type': 'fNIRS-CW',
         'model': 'optoHIVE'
     }
     EXPERIMENTER = 'optoHIVE Team'
+
+
+# Maximum threshold for channel rejection
+# Reject epochs based on maximum peak-to-peak (PTP) signal amplitude, i.e. the absolute difference between the lowest and the highest signal value.
+# If the PTP signal amplitude of any one channel exceeds the rejection threshold, the respective epoch will be dropped.
+REJECT_CRITERIA = {
+    'hbo': 80e-6
+}
+
+HB_CHANNEL_TYPES = ['hbo', 'hbr']
